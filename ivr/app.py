f11769a13ea20b74175b7fb59ac4c63de73741ef
Mariano Dominguez <mardom4164@gmail.com>
2025-03-05 14:41:30 -0300

    no rate

--- ivr/app.py
+++ ivr/app.py
@@ -9,14 +9,14 @@ app = Flask(__name__)
 @app.route('/voice', methods=['POST'])
 def voice():
     """Genera el TwiML para la llamada."""
-    mensaje = request.args.get('mensaje', 'Mensaje no proporcionado')
+    mensaje = request.args.get('mensaje', 'Mensaje no proporcionado') #Obtiene el parametro mensaje
     twiml = f'''<?xml version="1.0" encoding="UTF-8"?>
     <Response>
-        <Say language="es-ES" voice="alice" rate="85%">{mensaje}</Say>
+        <Say voice="alice" language="es-ES">{mensaje}</Say>
         <Gather input="dtmf" numDigits="1" action="/gather?mensaje={mensaje}">
-            <Say language="es-ES" rate="85%">Presiona 1 para escuchar el mensaje nuevamente.</Say>
+            <Say voice="alice" language="es-ES">Presiona 1 para escuchar el mensaje nuevamente.</Say>
         </Gather>
-        <Say language="es-ES" rate="85%">Gracias por tu llamada.</Say>
+        <Say voice="alice" language="es-ES">Gracias por tu llamada.</Say>
     </Response>
     '''
     return Response(twiml, mimetype='application/xml')
@@ -24,23 +24,23 @@ def voice():
 @app.route('/gather', methods=['POST'])
 def gather():
     """Maneja la entrada del usuario."""
-    mensaje = request.args.get('mensaje', 'Mensaje no proporcionado')
-    #if 'Digits' in request.form and request.form['Digits'] == '1':
-    twiml = f'''<?xml version="1.0" encoding="UTF-8"?>
-    <Response>
-        <Say language="es-ES" voice="alice" rate="85%">{mensaje}</Say>
-        <Gather input="dtmf" numDigits="1" action="/gather?mensaje={mensaje}">
-            <Say language="es-ES" rate="85%">Presiona 1 para escuchar el mensaje nuevamente.</Say>
-        </Gather>
-        <Say language="es-ES" rate="85%">Gracias por tu llamada.</Say>
-    </Response>
-    '''
-    # else:
-    #     twiml = '''<?xml version="1.0" encoding="UTF-8"?>
-    #     <Response>
-    #         <Say language="es-ES" rate="85%">Gracias por tu llamada.</Say>
-    #     </Response>
-    #     '''
+    mensaje = request.args.get('mensaje', 'Mensaje no proporcionado')#Obtiene el parametro mensaje
+    if 'Digits' in request.form and request.form['Digits'] == '1':
+        twiml = f'''<?xml version="1.0" encoding="UTF-8"?>
+        <Response>
+            <Say voice="alice" language="es-ES">{mensaje}</Say>
+            <Gather input="dtmf" numDigits="1" action="/gather?mensaje={mensaje}">
+                <Say voice="alice" language="es-ES">Presiona 1 para escuchar el mensaje nuevamente.</Say>
+            </Gather>
+            <Say voice="alice" language="es-ES">Gracias por tu llamada.</Say>
+        </Response>
+        '''
+    else:
+        twiml = '''<?xml version="1.0" encoding="UTF-8"?>
+        <Response>
+            <Say voice="alice" language="es-ES">Gracias por tu llamada.</Say>
+        </Response>
+        '''
     return Response(twiml, mimetype='application/xml')
 
 
