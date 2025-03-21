48da4636c42cee71f880728c3918708a11662c08
Mariano Dominguez <mariano.dominguez@ipsos.com>
2025-03-21 07:54:55 -0300

    cambiar mensaje final

--- ivr/app.py
+++ ivr/app.py
@@ -17,7 +17,7 @@ def voice():
         <Gather input="dtmf" numDigits="1" action="/gather?mensaje={mensaje_codificado}">
             <Say voice="alice" language="es-ES">Presiona 1 para escuchar el mensaje nuevamente.</Say>
         </Gather>
-        <Say voice="alice" language="es-ES">Gracias por tu llamada.</Say>
+        <Say voice="alice" language="es-ES">Gracias por su atencion.</Say>
     </Response>
     '''
     return Response(twiml, mimetype='application/xml')
@@ -34,13 +34,13 @@ def gather():
             <Gather input="dtmf" numDigits="1" action="/gather?mensaje={mensaje_codificado}">
                 <Say voice="alice" language="es-ES">Presiona 1 para escuchar el mensaje nuevamente.</Say>
             </Gather>
-            <Say voice="alice" language="es-ES">Gracias por tu llamada.</Say>
+            <Say voice="alice" language="es-ES">Gracias por su atencion.</Say>
         </Response>
         '''
     else:
         twiml = '''<?xml version="1.0" encoding="UTF-8"?>
         <Response>
-            <Say language="es-ES" rate="85%">Gracias por tu llamada.</Say>
+            <Say language="es-ES" rate="85%">Gracias por su atencion.</Say>
         </Response>
         '''
     return Response(twiml, mimetype='application/xml')
@@ -49,64 +49,3 @@ def gather():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-
-
-# from flask import Flask, request, Response
-# import os
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello_world():
-#     print("AAAAAAAAAAAAAAAAA")
-#     return 'Hola Mundo'
-
-
-
-# @app.route("/inicio", methods=['POST', 'GET'])
-# def inicio():
-#     print("BBBBBBBBBBBBBBBBBBB")
-#     mensaje = request.args.get('mensaje', 'Este es un mensaje por defecto.')
-#     print(mensaje)
-#     response = f"""
-#     <Response>
-#         <Gather numDigits="1" action="/procesar_opcion?mensaje={mensaje}", timeout="5" method="POST">
-#             <Say voice="alice" language="es-ES">{mensaje}</Say>
-#         </Gather>
-#         <Say voice="alice" language="es-ES">Gracias, adiós.</Say>
-#         <Hangup/>
-#     </Response>
-#     """
-#     return Response(response, content_type="text/xml")
-
-
-# @app.route("/procesar_opcion", methods=['POST'])
-# def procesar_opcion():
-#     digits = request.form.get('Digits')
-
-#     if digits == "1":
-#         # Si el usuario presiona 1, repetimos el mensaje
-#         response = f"""
-#         <Response>
-#             <Gather numDigits="1" action="/procesar_opcion" timeout="5" method="POST">
-#                 <Say voice="alice" language="es-ES">{mensaje}</Say>
-#             </Gather>
-#             <Say voice="alice" language="es-ES">Gracias, adiós.</Say>
-#             <Hangup/>
-#         </Response>
-#         """
-#     else:
-#         # Si presiona cualquier otra cosa o no presiona nada
-#         response = """
-#         <Response>
-#             <Say voice="alice" language="es-ES">Gracias, adiós.</Say>
-#             <Hangup/>
-#         </Response>
-#         """
-
-#     return Response(response, content_type="text/xml")
-
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
