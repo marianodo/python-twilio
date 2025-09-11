a4cde7ff7e45cdc45de3a7fb5f02f78e39ab30e6
Mariano Dominguez <mardom4164@gmail.com>
2025-09-11 21:14:56 +0100

    add whatsapp script

--- send_whatsapp_on_alarm.py
+++ send_whatsapp_on_alarm.py
@@ -12,6 +12,10 @@ from twilio.rest import Client
 from flask import Flask, jsonify
 from threading import Thread, Timer
 from datetime import datetime
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde .env
+load_dotenv()
 
 app = Flask(__name__)
 TEST_COUNT = 0
